0416a393d497596d97c5a3096417d98ec3e699a5
nguyenvandai <nguyenvandaicd@gmail.com>
2023-02-14 16:50:42 +0700

    add swagger

--- src/main/java/com/example/itspower/controller/EmployeeController.java
+++ src/main/java/com/example/itspower/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/api")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
